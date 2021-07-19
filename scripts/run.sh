fc1fbe716933a2f1d6979e2a2975ce19c43553ca
Dima Brusilovsky <dimabru@gmail.com>
2021-07-19 21:04:57 +0300

    feat: support any input

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,5 @@
-#! /bin/bash -e
+#! /bin/bash
 
-temp_manifest_name=/tmp/manifest.yaml
 helm_options=()
 datree_options=()
 helm_chart_location=""
@@ -9,31 +8,23 @@ eoo=0
 
 while [[ $1 ]]; do
     if ! ((eoo)); then
-        case "$1" in
-        version | help | --help | config)
-            $HELM_PLUGIN_DIR/bin/datree $@
-            exit
-            ;;
-        test)
-            datree_command="$1"
-            helm_chart_location="$2"
-            shift 2
-            ;;
-        --)
+        if [[ $1 == "--" ]]; then
             eoo=1
-            shift
-            ;;
-        *)
+        elif [[ $helm_chart_location == "" && $(helm show chart $1 2> /dev/null) != "" ]]; then
+            helm_chart_location=$1
+        else 
             datree_options+=("$1")
-            shift
-            ;;
-        esac
+        fi
+
+        shift
     else
         helm_options+=("$1")
         shift
     fi
 done
 
-helm template "$helm_chart_location" "${helm_options[@]}" > ${temp_manifest_name}
-
-$HELM_PLUGIN_DIR/bin/datree $datree_command "$temp_manifest_name" "${datree_options[@]}"
+if [[ ${helm_chart_location} != "" ]]; then
+    helm template "$helm_chart_location" "${helm_options[@]}" | $HELM_PLUGIN_DIR/bin/datree "${datree_options[@]}" -
+else
+    $HELM_PLUGIN_DIR/bin/datree "${datree_options[@]}"
+fi
