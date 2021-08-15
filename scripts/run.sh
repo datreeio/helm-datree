bb9687e4c90f04362643433c27a911af65e6892c
myishay <myishay@gmail.com>
2021-08-15 15:07:20 +0300

    refactor: variable renaming

--- scripts/run.sh
+++ scripts/run.sh
@@ -29,9 +29,9 @@ done
 
 if [[ ${helm_chart_location} != "" ]]; then
     currUinxTimestamp=$(date +%s)
-    tempDirPath="/tmp/${helm_chart_name}_$currUinxTimestamp.yaml"
-    helm template "${helm_options[@]}" "$helm_chart_location" > $tempDirPath
-    $HELM_PLUGIN_DIR/bin/datree "${datree_options[@]}" $tempDirPath
+    tempManifestPath="/tmp/${helm_chart_name}_$currUinxTimestamp.yaml"
+    helm template "${helm_options[@]}" "$helm_chart_location" > $tempManifestPath
+    $HELM_PLUGIN_DIR/bin/datree "${datree_options[@]}" $tempManifestPath
 else
     $HELM_PLUGIN_DIR/bin/datree "${datree_options[@]}"
 fi
