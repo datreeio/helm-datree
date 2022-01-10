97b6a66ecf4ab42fdd2e58556ca46b87610ce25f
geisterfurz007 <geisterfurz007@users.noreply.github.com>
2022-01-10 10:05:05 +0100

    feat: support other helm commands (#23) (#24)

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,13 +9,15 @@ helm_chart_name=""
 datree_command=""
 eoo=0
 
+helm_command=${DATREE_HELM_COMMAND:-helm}
+
 while [[ $1 ]]; do
     if ! ((eoo)); then
         if [[ $1 == "--" ]]; then
             eoo=1
-        elif [[ $helm_chart_location == "" && $(helm show chart $1 2> /dev/null | grep apiVersion) == apiVersion* ]]; then
+        elif [[ $helm_chart_location == "" && $($helm_command show chart $1 2> /dev/null | grep apiVersion) == apiVersion* ]]; then
             helm_chart_location=$1
-            helm_chart_name=$(helm show chart $1 | grep -o '^name: .*' | cut -c 7-)
+            helm_chart_name=$($helm_command show chart $1 | grep -o '^name: .*' | cut -c 7-)
         else
             datree_options+=("$1")
         fi
@@ -30,7 +32,7 @@ done
 if [[ ${helm_chart_location} != "" ]]; then
     currUinxTimestamp=$(date +%s)
     tempManifestPath="/tmp/${helm_chart_name}_$currUinxTimestamp.yaml"
-    helm template "${helm_options[@]}" "$helm_chart_location" > $tempManifestPath
+    $helm_command template "${helm_options[@]}" "$helm_chart_location" > $tempManifestPath
     $HELM_PLUGIN_DIR/bin/datree "${datree_options[@]}" $tempManifestPath
 else
     $HELM_PLUGIN_DIR/bin/datree "${datree_options[@]}"
